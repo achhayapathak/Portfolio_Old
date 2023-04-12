5a74330910a6325fab92d38470e1342c3cd176c0
Achhaya Pathak <achhayapathak11@gmail.com>
2023-04-12 22:08:08 +0530

    updated for deployment

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,13 +13,13 @@ const About = () => {
           </p>
         </div>
 
-        <p className="text-2xl mt-20 text-gray-400">
+        <p className="text-xl mt-20 text-gray-400">
         Hi, My name is Achhaya Pathak. With a passion for software development, I am currently pursuing my Masters in Mathematics and Computing from IIT Guwahati and I am an upcoming software developer at GIST Impact. In my current project, I am working on the optimal payment split in the Bitcoin Lightning Network. I keep learning about new technologies and in my spare time I like to play flute and tweet irrelevant stuff.
         </p>
 
         <br />
 
-        <p className="text-2xl text-gray-400">
+        <p className="text-xl text-gray-400">
         I've worked in a variety of fields, including machine learning, blockchain technology, full stack development, and web development. I've worked with a number of programming languages and technologies, including C++, C, Java, Javascript, and Python, as well as React, Tailwind CSS, and streamlit, among others. My experience with databases has been with MySQL and MongoDB. Please communicate with me if any of these technologies intrigues you.
         </p>
       </div>
