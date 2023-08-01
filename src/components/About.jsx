f502791e88b7629fc7cb7a0004c298fe21bf2cf2
Achhaya Pathak <93811742+achhayapathak@users.noreply.github.com>
2023-08-01 22:15:30 +0530

    Updated About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,11 +17,11 @@ const About = () => {
         Hey there! I'm Achhaya Pathak, a passionate and skilled developer specializing in creating innovative solutions. With a strong background in software development and a knack for problem-solving, I thrive on tackling complex challenges and turning them into elegant, user-friendly applications. Throughout my journey, I have honed my skills in various programming languages and technologies, allowing me to adapt and excel in diverse projects. I have expertise in full-stack application development, where I seamlessly integrate front-end technologies like HTML, CSS, and ReactJs with back-end frameworks like PHP and databases such as SQL and MongoDB.
         </p>
 
-        <br />
+{/*         <br /> */}
 
-        <p className="text-xl text-gray-400">
+{/*         <p className="text-xl text-gray-400">
         What sets me apart is my drive for continuous learning and staying up-to-date with the latest industry trends. I have a deep-rooted passion for exploring new technologies, from deep learning to blockchain, and leveraging them to build cutting-edge solutions. With a keen eye for detail, I strive to deliver high-quality code that not only meets functional requirements but also exceeds expectations in terms of performance and user experience. Whether it's collaborating with a team or taking ownership of a project, I am dedicated to delivering results that make a meaningful impact. If you're looking for a creative and adaptable developer with a passion for innovation, I would be thrilled to contribute my skills and expertise to your next venture.
-        </p>
+        </p> */}
       </div>
     </div>
   );
